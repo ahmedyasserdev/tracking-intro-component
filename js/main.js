b3f854e524fa842461b687ab1fcc1e62f43e1b26
ahmedyasserdev <ahmedyasser3033@gmail.com>
2023-10-30 21:56:05 +0200

    finished navbar

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const toggler = document.querySelector(".toggler")
+const nav = document.querySelector(".nav")
+
+
+toggler.addEventListener("click" , () => {
+    const currentSrc = toggler.getAttribute("src")
+    const burgerIcon = 'images/icon-hamburger.svg'
+    const closeIcon = 'images/icon-close.svg'
+    if (currentSrc === burgerIcon ) {
+        toggler.setAttribute("src" , closeIcon )
+    }else {
+        toggler.setAttribute('src', burgerIcon);
+
+    }
+
+    nav.classList.toggle("show")
+
+    
+}  )
